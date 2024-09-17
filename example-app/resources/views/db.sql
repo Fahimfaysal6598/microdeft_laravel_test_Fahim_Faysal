765e727357c6edd90a17e4b51292c866d5814194
Fahimfaysal6598 <fahim15-14052@diu.edu.bd>
2024-09-17 12:40:41 +0600

    name

---
+++ example-app/resources/views/db.sql
@@ -0,0 +1,17 @@
+create table cities{
+    "id"
+}
+
+
+
+insert into cities{
+    "id":id, "name":name, "status":status, "created_at":created_at, "updated_at":updated_at;
+}
+
+CREATE TABLE STUDENT{
+
+}
+
+INSERT INTO STUDENT{
+    "sid":id, "name":sname,
+}
